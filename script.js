4a2776bb7cdbc391fcc1fb05d6c18838a17512b8
Adrian Morales <58735890+adrianmorales12635@users.noreply.github.com>
2022-06-20 18:17:32 -0500

    corrected

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ var singleNumber = function(nums) {
         return nums[0]
     }
     for(var x = 0; x<nums.length; x++){
-        for(var y = 1; y<nums.length; y++){
-            if(x == y) {
-                break
+        count = 0
+        for(var y = 0; y<nums.length; y++){
+            if(x == y){
+                continue
             }
-            if(nums[x] == nums[y]) {
-                break
+            else if (nums[x] == nums[y]){
+                continue
             }
             else {
-                thenum = nums[x]
+                count++
             }
+            
+        }
+        if(count == nums.length - 1) {
+            return nums[x]
         }
     }
-    return thenum
 };
 
 var nums1 = [2,2,1]
 // output 1
-var nums2 = [4,1,4,1,2]
+var nums2 = [2,5,3,3,5]
 //output 4
 var nums3 = [1]
 //output 1
